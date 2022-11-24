c40ea21f4c5892fac1536f7f41f4078cd5bee665
andrade-felipe-dev <83104193+andrade-felipe-dev@users.noreply.github.com>
2022-11-24 20:27:06 -0300

    commit
    alterado a forma de visualização, adicionado a logo tipo do projeto

--- login-frontend/src/views/credentials/Index.vue
+++ login-frontend/src/views/credentials/Index.vue
@@ -7,21 +7,28 @@
 
         </v-flex>
       </v-layout>
-      <v-layout align-center justify-start>
+      <v-layout align-center justify-center>
         <v-flex xs12 sm15 md10>
-          <v-card flat>
-            <v-row>
-              <v-col>
-                <v-text-field  clearable outlined class="mt-2" label="Usuário" />
+          <v-card elevation="12" max-width="500" max-height="600">
+            <v-row class="d-flex justify-center">
+              <v-col cols="10" class="pb-0 pt-3">
+                <v-text-field clearable outlined class="mt-2 " label="Usuário" />
               </v-col>
             </v-row>
-            <v-row>
-              <v-col>
+            <v-row class="d-flex justify-center">
+              <v-col cols="10" class="pb-0 pt-0">
                 <v-text-field  clearable outlined label="Senha" type="password" />
               </v-col>
             </v-row>
-            <v-row>
-              <v-col cols="12">
+            <v-row class="d-flex justify-center">
+              <v-col class="pb-0 pt-0" cols="10">
+                <v-btn
+                  block
+                  color="primary">
+                  Acessar
+                </v-btn>
+              </v-col>
+              <v-col class="pb-0 pt-0 d-flex justify-center" cols="10">
                 <v-btn
                   color="primary"
                   plain
@@ -29,9 +36,13 @@
                 >
                   Esqueceu sua senha?
                 </v-btn>
-                <v-btn
-                  color="primary">
-                  Acessar
+              </v-col>
+            </v-row>
+            <v-divider class="mt-3"></v-divider>
+            <v-row class="d-flex justify-center mt-3">
+              <v-col cols="10" class="d-flex justify-center mt-3">
+                <v-btn color="green" class="white--text">
+                  Criar novo usuário
                 </v-btn>
               </v-col>
             </v-row>
